b738e076e70a3f8b70621e25e888aa55b50213b8
saulm23 <marcasaul00@gmail.com>
2026-07-08 19:11:56 -0400

    solved ch1 ex10

---
+++ c1/10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int c;
+	while((c = getchar()) != EOF)
+	{
+		if(c == '\t') printf("\\t");
+		if(c == '\b') printf("\\b");
+		if(c == '\\') printf("\\\\");
+	}
+	return 0;
+}
