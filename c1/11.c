8601bee632b28bf74bad43ed639c2b028d65e00e
saulm23 <marcasaul00@gmail.com>
2026-07-13 08:44:56 -0400

    solved ch1 ex10

---
+++ c1/11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main()
+{
+	int c, state, nl, nw, nc;
+	state = OUT;
+	nl = nw = nc = 0;
+	while((c = getchar()) != EOF)
+	{
+		++nc;
+		if(c == '\n') ++nl;
+		if(c == ' ' || c == '\n' || c == '\t')
+		{
+			state = OUT;
+		}
+		else if (state == OUT)
+		{
+			++nw;
+			state = IN;
+		}
+	}	
+	printf("%d %d% d\n", nl, nw, nc);
+	return 0;
+}
